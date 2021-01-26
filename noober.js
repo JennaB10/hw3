79ed9e5532e22f495d0bc1fd59b829ddf36b153d
JennaB10 <jennb1046@gmail.com>
2021-01-25 19:43:11 -0600

    passengerdet

--- noober.js
+++ noober.js
@@ -26,42 +26,72 @@ function pageLoad() {
   }
 
   console.log(levelOfService)
-
-  if (ride.legnth = 1) {
+  console.log(ride.length)
+ 
+  if(ride.length == 1) {
   passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
   passenger1Phone = ride[0].passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = ride[0].numberOfPassengers
   passenger1PickupAddressLine1 = ride[0].pickupLocation.address
   passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
   passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
   passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
-  passenger1NumberOfPassengers = ride[0].dropoffLocation.numberOfPassengers
   passenger1NooberPurple = ride[0].purpleRequested
-  
-}
-  else if (ride.length >1) {
+
+} else if (ride.length == 2) {
+
+  passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
+  passenger1Phone = ride[0].passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = ride[0].numberOfPassengers
+  passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+  passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
+  passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+  passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
+  passenger1NooberPurple = ride[0].purpleRequested
+
   passenger2Name = ride[1].passengerDetails.first + ' ' + ride[1].passengerDetails.last
   passenger2Phone = ride[1].passengerDetails.phoneNumber
+  passenger2NumberOfPassengers = ride[1].numberOfPassengers
   passenger2PickupAddressLine1 = ride[1].pickupLocation.address
   passenger2PickupAddressLine2 = ride[1].pickupLocation.city + ', ' + ride[1].pickupLocation.state + ' ' + ride[1].pickupLocation.zip
   passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
   passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + ', ' + ride[1].dropoffLocation.state + ' ' + ride[1].dropoffLocation.zip  
-  passenger2NumberOfPassengers = ride[1].dropoffLocation.numberOfPassengers
   passenger2NooberPurple = ride[1].purpleRequested
-}
 
-  else (ride.length >2) {
+} else {
+
+  passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
+  passenger1Phone = ride[0].passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = ride[0].numberOfPassengers
+  passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+  passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
+  passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+  passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
+  passenger1NooberPurple = ride[0].purpleRequested
+  
+  passenger2Name = ride[1].passengerDetails.first + ' ' + ride[1].passengerDetails.last
+  passenger2Phone = ride[1].passengerDetails.phoneNumber
+  passenger2NumberOfPassengers = ride[1].numberOfPassengers
+  passenger2PickupAddressLine1 = ride[1].pickupLocation.address
+  passenger2PickupAddressLine2 = ride[1].pickupLocation.city + ', ' + ride[1].pickupLocation.state + ' ' + ride[1].pickupLocation.zip
+  passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
+  passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + ', ' + ride[1].dropoffLocation.state + ' ' + ride[1].dropoffLocation.zip  
+  passenger2NooberPurple = ride[1].purpleRequested
+
   passenger3Name = ride[2].passengerDetails.first + ' ' + ride[2].passengerDetails.last
   passenger3Phone = ride[2].passengerDetails.phoneNumber
+  passenger3NumberOfPassengers = ride[2].numberOfPassengers
   passenger3PickupAddressLine1 = ride[2].pickupLocation.address
   passenger3PickupAddressLine2 = ride[2].pickupLocation.city + ', ' + ride[2].pickupLocation.state + ' ' + ride[2].pickupLocation.zip
   passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
   passenger3DropoffAddressLine2 = ride[2].dropoffLocation.city + ', ' + ride[2].dropoffLocation.state + ' ' + ride[2].dropoffLocation.zip
-  passenger3NumberOfPassengers = ride[2].dropoffLocation.numberOfPassengers
   passenger3NooberPurple = ride[2].purpleRequested
 
-  console.log(numberOfPassengers)
-
 }
+  
+
+ 
+
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
 
